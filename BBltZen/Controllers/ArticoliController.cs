4df8130972540c92ab07c526773df8b44f7967b0
Maurizio Chieruzzi <maurizio.chieruzzi@gmail.com>
2025-09-12 22:02:01 +0200

    Rimossi file default WeatherForecast, aggiunto ArticoliController, fix database context

---
+++ BBltZen/Controllers/ArticoliController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Database;
+
+namespace BBltZen.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArticoliController : ControllerBase
+    {
+        private readonly BubbleTeaContext _context;
+
+        public ArticoliController(BubbleTeaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Articoli
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Articolo>>> GetArticoli()
+        {
+            return await _context.Articolo.ToListAsync();
+        }
+
+        // GET: api/Articoli/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Articolo>> GetArticolo(int id)
+        {
+            var articolo = await _context.Articolo.FindAsync(id);
+
+            if (articolo == null)
+            {
+                return NotFound();
+            }
+
+            return articolo;
+        }
+    }
+}
--- BBltZen/Program.cs
+++ BBltZen/Program.cs
@@ -15,8 +15,7 @@ namespace BBltZen
 
             // Add services to the container.
 
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddControllers();            
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
